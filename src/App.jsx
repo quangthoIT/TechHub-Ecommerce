c093cd7728067318ec3763e8861df4fd33f594a0
quangthoIT <quangthoIT04@gmail.com>
2025-08-14 10:09:31 +0700

    Routing Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl text-red-500">Hello TechHub Ecommerce</h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+  return (
+    <div className="bg-white py-3 border border-b border-gray-200">Navbar</div>
+  );
+};
+
+export default Navbar;
