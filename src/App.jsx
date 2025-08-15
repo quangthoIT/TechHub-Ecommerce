7a884153fb11b36ab9449a18e0e47fdc0e9736e7
quangthoIT <quangthoIT04@gmail.com>
2025-08-15 16:01:00 +0700

    Complete Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
@@ -5,11 +6,74 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
 const App = () => {
+  const [location, setLocation] = useState();
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const [loadingLocation, setLoadingLocation] = useState(false);
+  const [locationError, setLocationError] = useState("");
+
+  const getLocation = async () => {
+    setLoadingLocation(true);
+    setLocationError(""); // Reset lỗi cũ
+
+    // Kiểm tra xem trình duyệt có hỗ trợ định vị hay không
+    if (!navigator.geolocation) {
+      setLocationError("The browser does not support geolocation.");
+      setLoadingLocation(false);
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const { latitude, longitude } = position.coords;
+        // console.log(latitude, longitude);
+        const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+
+        try {
+          const location = await axios.get(url);
+          const exactLocation = location.data.address;
+          setLocation(exactLocation);
+          setOpenDropdown(false); // Đóng mở Dropdown
+          // console.log(exactLocation);
+        } catch {
+          setLocationError("Unable to get address from server."); // Lỗi khi lấy vị trí
+        } finally {
+          setLoadingLocation(false);
+        }
+      },
+      (err) => {
+        if (err.code === 1) {
+          setLocationError("You have denied location access."); // Lỗi khi người dùng khóa truy cập vị trí
+        } else if (err.code === 2) {
+          setLocationError("Location cannot be determined."); // Không thể xác định vị trí
+        } else if (err.code === 3) {
+          setLocationError(
+            "The request to get the location took too long and was canceled."
+          ); // Yêu cầu lấy vị trí quá lâu, đã bị hủy.
+        } else {
+          setLocationError("Unknown error while getting location."); // Lỗi không xác định khi lấy vị trí.
+        }
+        setLoadingLocation(false);
+      }
+    );
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar
+        location={location}
+        getLocation={getLocation}
+        openDropdown={openDropdown}
+        setOpenDropdown={setOpenDropdown}
+        loadingLocation={loadingLocation}
+        locationError={locationError}
+      />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/products" element={<Products />}></Route>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,140 @@
-const Navbar = () => {
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { MapPin, ShoppingCart } from "lucide-react";
+import { CgClose } from "react-icons/cg";
+import { FaCaretDown } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = ({
+  location,
+  getLocation,
+  openDropdown,
+  setOpenDropdown,
+  loadingLocation,
+  locationError,
+}) => {
+  const toggleDropdown = () => {
+    setOpenDropdown(!openDropdown);
+  };
   return (
-    <div className="bg-white py-3 border border-b border-gray-200">Navbar</div>
+    <div className="bg-white py-3 shadow-2xl">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <div className="flex gap-7 items-center">
+          <Link to="/">
+            <h1 className="font-bold text-3xl">
+              <span className="text-red-500 font-serif">T</span>echHub
+            </h1>
+          </Link>
+          <div className="flex gap-2 cursor-pointer text-gray-700 items-center">
+            <MapPin className="text-red-500" />
+            <span className="font-semibold ">
+              {location ? (
+                <div className="-space-y-2">
+                  <p>{location.country}</p>
+                  <p>{location.city}</p>
+                </div>
+              ) : (
+                "Add Address"
+              )}
+            </span>
+            <FaCaretDown onClick={toggleDropdown} />
+          </div>
+          {openDropdown ? (
+            <div className="w-[230px] h-max shadow-2xl z-50 bg-white fixed top-16 left-60 border-2 p-4 border-gray-100 rounded-md">
+              <h1 className="font-semibold mb-4 text-xl flex justify-between items-center">
+                Change Location
+                <span onClick={toggleDropdown} className="cursor-pointer">
+                  <CgClose />
+                </span>
+              </h1>
+              <button
+                onClick={getLocation}
+                className="bg-red-500 text-white p-2 rounded-md cursor-pointer hover:bg-red-600 transition-all"
+              >
+                {loadingLocation ? "Taking position..." : "Detect My Location"}
+              </button>
+
+              {/* Thông báo lỗi lấy vị trí */}
+              {locationError && (
+                <p className="text-red-500 text-base mt-2">{locationError}</p>
+              )}
+            </div>
+          ) : null}
+        </div>
+        {/* Menu */}
+        <nav className="flex gap-7 items-center">
+          <ul className="flex gap-7 items-center text-xl font-semibold ">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "text-red-500 border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to={"/products"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "text-red-500 border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Products</li>
+            </NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "text-red-500 border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "text-red-500 border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+          <Link to={"/cart"} className="relative">
+            <ShoppingCart className="h-7 w-7" />
+            <span className="bg-red-500 px-2 rounded-full absolute -top-3 -right-3 text-white">
+              0
+            </span>
+          </Link>
+          <div className="flex items-center">
+            <SignedOut>
+              <SignInButton className="bg-red-500 text-white px-3 py-1 rounded-md cursor-pointer" />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </nav>
+      </div>
+    </div>
   );
 };
 
