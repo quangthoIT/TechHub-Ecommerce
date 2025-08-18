8241efbb15efeb6467df150e6200ada5f9c93aac
quangthoIT <quangthoIT04@gmail.com>
2025-08-18 22:36:18 +0700

    Complete Mid-Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Carousel from "../components/Carousel";
+import MidBanner from "../components/MidBanner";
 
 const Home = () => {
   return (
     <>
       <Carousel />
+      <MidBanner />
     </>
   );
 };
