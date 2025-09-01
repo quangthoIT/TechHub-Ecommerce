e26f55dcbd45190eb3a866bb08443cd6bc483c54
quangthoIT <quangthoIT04@gmail.com>
2025-09-01 10:35:36 +0700

    My Cart, Delivery Info, Oder Summary, Empty Cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,11 +5,46 @@ export const CartContext = createContext(null);
 export const CartProvider = ({ children }) => {
   const [cartItem, setCartItem] = useState([]);
   const addtoCart = (product) => {
-    setCartItem([...cartItem, product]);
-    console.log("Product added to cart:", cartItem);
+    const itemInCart = cartItem.find((item) => item.id === product.id);
+    if (itemInCart) {
+      // Increase the quantity of the existing item in the cart
+      const updatedCart = cartItem.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      setCartItem(updatedCart);
+    } else {
+      // Add the new item to the cart
+      setCartItem([...cartItem, { ...product, quantity: 1 }]);
+    }
   };
+
+  const updateQuantity = (productId, action) => {
+    setCartItem(
+      cartItem
+        .map((item) => {
+          if (item.id === productId) {
+            let newUnit = item.quantity;
+            if (action === "increase") {
+              newUnit = item.quantity + 1;
+            } else if (action === "decrease") {
+              newUnit = item.quantity - 1;
+            }
+            return newUnit > 0 ? { ...item, quantity: newUnit } : null;
+          }
+          return item;
+        })
+        .filter((item) => item !== null)
+    );
+  };
+
+  const deleteItem = (productId) => {
+    setCartItem(cartItem.filter((item) => item.id !== productId));
+  };
+
   return (
-    <CartContext.Provider value={{ cartItem, setCartItem, addtoCart }}>
+    <CartContext.Provider
+      value={{ cartItem, setCartItem, addtoCart, updateQuantity, deleteItem }}
+    >
       {children}
     </CartContext.Provider>
   );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,19 @@
-import { Trash2 } from "lucide-react";
+import { NotebookText, ShoppingBag, Trash2, Truck } from "lucide-react";
 import { useCart } from "../context/CartContext";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
+import emptyCart from "../assets/empty-cart.png";
 
 const Cart = () => {
-  const { cartItem } = useCart();
+  const { cartItem, updateQuantity, deleteItem } = useCart(); // Destructure cartItem from the useCart hook
+  const { user } = useUser(); // Destructure user from the useUser hook
+
+  const totalPrice = cartItem.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  ); // Tính tống gia cái trong giỏ hàng
+
   const navigate = useNavigate();
 
   // State cho địa chỉ giao hàng
@@ -88,6 +97,7 @@ const Cart = () => {
                     key={index}
                     className="bg-gray-100 p-5 rounded-xl flex items-center justify-between mt-3 w-full"
                   >
+                    {/* Product */}
                     <div className="flex items-center gap-4">
                       <img
                         src={item.image}
@@ -97,18 +107,31 @@ const Cart = () => {
                       <div>
                         <h1 className="w-[300px] line-clamp-2">{item.title}</h1>
                         <p className="font-semibold text-red-500 text-lg">
-                          ${item.price}
+                          ${item.price * item.quantity}
                         </p>
                       </div>
                     </div>
-
+                    {/* Quantity */}
                     <div className="bg-red-500 text-white flex gap-4 p-2 rounded-md font-bold text-xl">
-                      <button className="cursor-pointer">-</button>
-                      <span className="text-lg">1</span>
-                      <button className="cursor-pointer">+</button>
+                      <button
+                        className="cursor-pointer"
+                        onClick={() => updateQuantity(item.id, "decrease")}
+                      >
+                        -
+                      </button>
+                      <span className="text-lg">{item?.quantity}</span>
+                      <button
+                        className="cursor-pointer"
+                        onClick={() => updateQuantity(item.id, "increase")}
+                      >
+                        +
+                      </button>
                     </div>
-
-                    <div className="text-red-500 cursor-pointer hover:text-red-600 transition-all hover:shadow-2xl p-2 rounded-full">
+                    {/* Delete */}
+                    <div
+                      className="text-red-500 cursor-pointer hover:text-red-600 transition-all hover:shadow-2xl p-2 rounded-full"
+                      onClick={() => deleteItem(item.id)}
+                    >
                       <Trash2 className="w-6 h-6" />
                     </div>
                   </div>
@@ -118,9 +141,9 @@ const Cart = () => {
 
             {/* Delivery Info */}
             <div className="grid grid-cols-2 gap-10 ">
-              <div className="bg-gray-100 rounded-md p-5 mt-5 space-y-2">
+              <div className="bg-gray-100 rounded-md p-5 mt-5 space-y-2 shadow-md">
                 <h1 className="font-bold text-xl">Delivery Info</h1>
-
+                {/* Full Name + Phone */}
                 <div className="flex gap-4 mt-4">
                   <div className="flex flex-col space-y-1 w-full">
                     <label htmlFor="" className="text-gray-900 font-semibold">
@@ -129,11 +152,12 @@ const Cart = () => {
                     <input
                       type="text"
                       placeholder="Enter your full name"
-                      value={fullName}
+                      value={user.fullName}
                       onChange={(e) => setFullName(e.target.value)}
                       className="bg-gray-50 border border-gray-200 rounded-md p-2 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder:text-gray-500 placeholder:transition-opacity placeholder:duration-800 focus:placeholder:opacity-0"
                     ></input>
                   </div>
+
                   <div className="flex flex-col space-y-1 w-full">
                     <label htmlFor="" className="text-gray-900 font-semibold">
                       Phone Number <span className="text-red-500">*</span>
@@ -147,7 +171,7 @@ const Cart = () => {
                     ></input>
                   </div>
                 </div>
-
+                {/* Province + Ward */}
                 <div className="flex gap-4 mt-4">
                   <div className="flex flex-col space-y-1 w-full">
                     {/* Dropdown chọn Tỉnh/Thành phố */}
@@ -191,7 +215,7 @@ const Cart = () => {
                     </select>
                   </div>
                 </div>
-
+                {/* Detailed Address */}
                 <div className="flex flex-col space-y-1 mt-4">
                   <label htmlFor="" className="text-gray-900 font-semibold">
                     Detailed Address <span className="text-red-500">*</span>
@@ -204,11 +228,11 @@ const Cart = () => {
                     className="bg-gray-50 border border-gray-200 rounded-md p-2 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder:text-gray-500 placeholder:transition-opacity placeholder:duration-800 focus:placeholder:opacity-0"
                   />
                 </div>
-
+                {/* Back + Checkout */}
                 <div className="flex justify-end gap-2 mt-5">
                   <button
                     onClick={() => navigate(-1)}
-                    className="bg-white text-red-500 py-2 px-4 rounded-md hover:bg-red-50 transition-all duration-300 cursor-pointer"
+                    className="bg-white text-black border border-gray-200 px-4 py-2 rounded-md cursor-pointer hover:text-white hover:bg-red-600 transition-all duration-300"
                   >
                     Back
                   </button>
@@ -222,19 +246,87 @@ const Cart = () => {
               </div>
 
               {/* Order Summary */}
-              <div className="bg-white rounded-md p-5 mt-5 space-y-2 border border-gray-200 shadow-xl h-max">
+              <div className="bg-white rounded-md p-5 mt-5 space-y-2 border border-gray-100 shadow-md h-max">
                 <h1 className="font-bold text-xl">Order Summary</h1>
+                {/* Total */}
+                <div className="flex gap-2 justify-between items-center text-gray-900 mt-3">
+                  <h1 className="flex gap-2 items-center text-gray-900">
+                    <span>
+                      <NotebookText />
+                    </span>
+                    Items Total
+                  </h1>
+                  <p>${totalPrice}</p>
+                </div>
+                {/* Delivery */}
+                <div className="flex gap-2 justify-between items-center text-gray-900 mt-3">
+                  <h1 className="flex gap-2 items-center">
+                    <span>
+                      <Truck />
+                    </span>
+                    Delivery Charge
+                  </h1>
+                  <p className="text-red-500 font-semibold">
+                    <span className="text-gray-900 line-through">$25</span> Free
+                  </p>
+                </div>
+                {/* Handling */}
+                <div className="flex gap-2 justify-between items-center text-gray-900 mt-3">
+                  <h1 className="flex gap-2 items-center">
+                    <span>
+                      <ShoppingBag />
+                    </span>
+                    Handling Charge
+                  </h1>
+                  <p className="text-red-500 font-semibold">$5</p>
+                </div>
+                {/* Grand Total */}
+                <hr className="text-gray-200 mt-3" />
+                <div className="flex gap-2 justify-between items-center text-gray-900 font-semibold text-lg mt-3">
+                  <h1>Grand Total</h1>
+                  <p>${totalPrice + 5}</p>
+                </div>
+                {/* Promo Code*/}
+                <div>
+                  <h1 className="font-semibold text-gray-700 mb-3 mt-4">
+                    Apply Promo Code
+                  </h1>
+                  <div className="flex gap-2">
+                    <input
+                      type="text"
+                      placeholder="Enter code"
+                      className="p-2 rounded-md w-full border border-gray-200 focus:outline-none focus:ring-1 focus:ring-gray-400
+                      placeholder:text-gray-500 placeholder:transition-opacity placeholder:duration-800 focus:placeholder:opacity-0"
+                    />
+                    <button className="bg-white text-black border border-gray-200 px-4 py-2 rounded-md cursor-pointer hover:bg-red-600  hover:text-white transition-all duration-300">
+                      Apply
+                    </button>
+                  </div>
+                </div>
+                <button className="w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition-all duration-300 cursor-pointer mt-3">
+                  Proceed to Checkout
+                </button>
               </div>
             </div>
           </div>
         </div>
       ) : (
-        <div>Cart Is Emper</div>
+        // Empty Cart
+        <div className="flex flex-col gap-3 justify-between items-center h-[600px]">
+          <h1 className="text-4xl text-red-500 font-bold text-muted">
+            Oh no! Your cart is empty
+          </h1>
+          <img src={emptyCart} alt="empty Cart" className="w-[450px]" />
+          <button
+            className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-md cursor-pointer"
+            onClick={() => navigate("/products")}
+          >
+            Continue Shopping
+          </button>
+        </div>
       )}
     </div>
   );
 };
 
 export default Cart;
-
-
